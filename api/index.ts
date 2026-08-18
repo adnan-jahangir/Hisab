15b1730a34acae109cb52ac8b31c3ceec2bce446
Adnan Jahangir <adnan.jahangir@iiuc.ac.bd>
2026-08-18 22:06:21 +0600

    fix: await connectDB() in Vercel Serverless API handler to eliminate HTTP 500 login errors

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,11 @@
 import app from '../server/index';
+import { connectDB } from '../server/config/db';
 
-export default app;
+export default async function handler(req: any, res: any) {
+  try {
+    await connectDB();
+  } catch (error) {
+    console.error('Database connection error in Vercel Serverless handler:', error);
+  }
+  return app(req, res);
+}
